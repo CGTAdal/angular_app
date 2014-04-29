0abd98a2ff0958123c051568e3f1b0649cc42377
Arpit Jalan <arpit@ajalan.info>
2014-04-29 12:06:03 +0530

    Completed CRUD

--- app/assets/javascripts/ngapp/controllers/dashboard.js
+++ app/assets/javascripts/ngapp/controllers/dashboard.js
@@ -2,6 +2,7 @@
 
 var dashbord = ngApp.controller('DashboardCtrl', function($scope, Company, $rootScope, $routeParams, $modal) {
   $rootScope.companies = Company.all();
+  // console.log($rootScope.companies);
 
   // open modal
   $scope.deleteCompany = function (company) {
@@ -15,14 +16,38 @@ var dashbord = ngApp.controller('DashboardCtrl', function($scope, Company, $root
   	// console.log($rootScope.companies);
 	  var modalInstance = $modal.open({
 	    backdrop: 'static',
-	    // param: $routeParams,
 	    templateUrl: '/templates/edit_company.html',
-	    controller: 'DashboardCtrl',
+	    controller: 'ModalInstanceCtrl',
       resolve: {
         company: function () {
-          return $rootScope.companies[$rootScope.companies.indexOf(company)];
+          return company;
         }
       }
 	  });
   };
 });
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+var ModalInstanceCtrl = function ($scope, $modalInstance, company, Company) {
+  var oldCompany = company;
+  $scope.company = company; //{name:'sdfsdfsdfs',url:'554554'}; //company;
+  // console.log(company+'\n\n'+$scope.company);
+
+  $scope.updateCompany = function() {
+    var attr = {};
+    attr = $scope.company;
+    // save company to db
+    var newCompany = Company.update(attr);
+    // close pop-up
+    $scope.$close();
+    // add notification
+    // Notification.add("success", "Company submitted successfully!");
+  };
+
+  $scope.cancel = function () {
+    // console.log(JSON.stringify(oldCompany));
+    $scope.company = oldCompany;
+    $modalInstance.dismiss('cancel');
+  };
+};
